1a3aaaa2346f479c1c744fab9e0b80dc666ef402
javjm <javjm@outlook.com>
2025-09-01 22:45:14 +0100

    Print the original message when the decrypted cipher text and the plain text match

--- one_time_pad_encryption.py
+++ one_time_pad_encryption.py
@@ -4,9 +4,8 @@ import random
 KEY = []
 
 
-def create_key():
-    plain_text = input('Enter value' + '\n')
-    plain_text = plain_text.replace(' ', '')
+def create_key(user_input):
+    plain_text = user_input.replace(' ', '')
 
     for letter in plain_text:
         KEY.append(random.choice(string.ascii_letters))
@@ -32,22 +31,22 @@ def convert_plain_text_and_key(plain_text, key):
 
 def encrypt(plain_text, key):
     # Here I'm performing a bitwise XOR operation on the plain text and the key to encrypt the data
-    print('Encrypting in progress' + '\n')
+    print('Encryption in progress' + '\n')
     plain_text_value, key_value = plain_text, key
-    cipher_text = []
+    cipher_text_binary = []
 
     for i, j in zip(plain_text_value, key_value):
         if int(i) ^ int(j):
-            cipher_text.append('1')
+            cipher_text_binary.append('1')
         else:
-            cipher_text.append('0')
-    cipher_text = ''.join(cipher_text)
-    return cipher_text
+            cipher_text_binary.append('0')
+    cipher_text_binary = ''.join(cipher_text_binary)
+    return cipher_text_binary
 
 
 def decrypt(cipher_text, key):
     # Here I'm performing a bitwise XOR operation on the cipher text and the key to decrypt the data
-    print('Decrypting in progress' + '\n')
+    print('Decryption in progress' + '\n')
     cipher_text_value, key_value = cipher_text, key
     plain_text = []
 
@@ -65,12 +64,19 @@ def binary_to_string(binaryString):
 
 
 def main():
-    plain_text, key = create_key()
+    user_input = input('Enter value' + '\n')
+    plain_text, key = create_key(user_input)
     plain_text_binary, key_binary = convert_plain_text_and_key(plain_text, key)
     cipher_text_binary = encrypt(plain_text_binary, key_binary)
-    print(f'One time pass cipher text with the string "{plain_text} - "' + cipher_text_binary)
+
+    print(f'Cipher text of the user input "{user_input}": "{binary_to_string(cipher_text_binary)}"')
     decrypted_plain_text_binary = decrypt(cipher_text_binary, key_binary)
-    print(binary_to_string(decrypted_plain_text_binary))
+    if decrypted_plain_text_binary == plain_text_binary:
+        print(user_input)
+    else:
+        raise Exception('The plain text message and the decrypted version of that message do not match')
+
+
 
 
 if __name__ == "__main__":
